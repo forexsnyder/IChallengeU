31e666eae84f823128393b399a1f1406023997c6
jvsnyder <forexsnyder@gmail.com>
2020-09-04 07:57:38 -0700

    added current comp

---
+++ client/ichallengeu/src/components/leaderboards.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Landingpage() {
+  return (
+    <div>
+
+
+    </div>
+  );
+}
+
+export default Landingpage;
---
+++ client/ichallengeu/src/screens/Landingpage/Landingpage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Homepage.css"
+
+import Leaderboards from '../../components/leaderboards'
+
+
+function Landingpage() {
+  return (
+    <div>
+
+
+    </div>
+  );
+}
+
+export default Landingpage;
